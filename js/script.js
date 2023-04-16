e68086331aa02d3d53d2c9a30203be1f794d6797
Melissa W <118938176+missywalker@users.noreply.github.com>
2023-04-16 10:57:03 -0400

    Initial Commit

---
+++ js/script.js
@@ -0,0 +1,23 @@
+const secondHand = document.querySelector('.second-hand');
+const minsHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
+
+function setDate() {
+  const now = new Date();
+
+  const seconds = now.getSeconds();
+  const secondsDegrees = ((seconds / 60) * 360) + 90;  //the + 90 offsets the 90 degree rotate we set for the hand in CSS.
+  secondHand.style.transform = `rotate(${secondsDegrees}deg)`; 
+
+  const mins = now.getMinutes();
+  const minsDegrees = (( mins / 60) * 360) + 90;
+  minsHand.style.transform = `rotate(${minsDegrees}deg)`;
+
+  const hours = now.getHours();
+  const hoursDegrees = ((hours / 12) * 360) + 90;
+  hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+
+
+}
+
+setInterval(setDate, 1000);
